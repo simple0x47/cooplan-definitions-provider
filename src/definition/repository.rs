1d20a6e73cc1eb5300c19e5feddf6a40cadd6690
Gabriel Ami <gabriel@simpleg.eu>
2022-10-01 17:26:05 +0300

    - unused logic

--- src/definition/repository.rs
+++
@@ -1,7 +0,0 @@
-use cooplan_definitions_lib::validated_source_category::ValidatedSourceCategory;
-
-use crate::error::Error;
-
-pub trait Repository {
-    fn read_all(&self) -> Result<Vec<ValidatedSourceCategory>, Error>;
-}
